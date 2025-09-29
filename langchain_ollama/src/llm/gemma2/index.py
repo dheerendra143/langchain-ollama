b85bf443e8a4415e07f57dbf6cbab8a7cdbb50c2
dheerendra143 <singhdheerendra83@gmail.com>
2025-09-29 19:14:26 +0530

    Created a llm folder

--- langchain_ollama/src/llm/gemma2/index.py
+++ langchain_ollama/src/llm/gemma2/index.py
@@ -1,8 +1,8 @@
-from langchain_community.llms import Ollama
+from langchain_community.chat_models import ChatOllama
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
-llm = Ollama(model="gemma:2b")
+llm = ChatOllama(model="gemma:2b")
 # prompt = ChatPromptTemplate.from_messages([
 #     ("system", "You are a helpful AI assistant."),
 #     ("user", "{input}")
