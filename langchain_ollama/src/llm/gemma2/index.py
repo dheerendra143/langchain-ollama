6841bc29483b3de8c1470f23a40f9114659d0a3f
dheerendra143 <singhdheerendra83@gmail.com>
2025-09-29 19:10:30 +0530

    Created a llm folder

--- langchain_ollama/src/llm/gemma2/index.py
+++ langchain_ollama/src/llm/gemma2/index.py
@@ -3,10 +3,10 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
 llm = Ollama(model="gemma:2b")
-prompt = ChatPromptTemplate.from_messages([
-    ("system", "You are a helpful AI assistant."),
-    ("user", "{input}")
-])
-output_parser = StrOutputParser()
+# prompt = ChatPromptTemplate.from_messages([
+#     ("system", "You are a helpful AI assistant."),
+#     ("user", "{input}")
+# ])
+# output_parser = StrOutputParser()
 
-chain = prompt | llm | output_parser
+chain = llm
--- langchain_ollama/src/modules/llm/routes/llm_routes.py
+++ langchain_ollama/src/modules/llm/routes/llm_routes.py
@@ -7,16 +7,13 @@ from ..validations.create_user import Usercreate
 from langchain_ollama.src.llm.gemma2.index import chain
 from langchain_ollama.src.modules.llm.models import llm
 
-
 routers = APIRouter()
-
-ollama_api_url = "http://localhost:11434/api/generate"
-
-
 
 @routers.post("/generate", status_code= status.HTTP_200_OK)
 def create_llm(request: llm.PromptRequest):
-    response = chain.invoke({"input": request.prompt})
+    print(request.prompt)
+    # response = chain.invoke({"input": request.prompt})
+    response = chain.invoke(request.prompt)
     return {"response": response}
 
 
